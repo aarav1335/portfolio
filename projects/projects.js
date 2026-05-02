3a243447ebea838b330afc9b51727102be8bf678
Aarav Agrawal <a7agrawal@ucsd.edu>
2026-05-01 17:10:49 -0700

    step 2 done?

--- projects/projects.js
+++ projects/projects.js
@@ -1,7 +1,29 @@
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 import { fetchJSON, renderProjects } from '/portfolio/global.js';
 const projects = await fetchJSON('/portfolio/lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 const title = document.querySelector('.projects-title');
 title.textContent = `${projects.length} Projects`;
-
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
+let sliceGenerator = d3.pie().value((d) => d.value);
+let arcData = sliceGenerator(data);
+let arcs = arcData.map((d) => arcGenerator(d));
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+arcs.forEach((arc, idx) => {
+    d3.select('#projects-pie-plot').append('path').attr('d', arc).attr('fill', colors(idx));
+});
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+  legend
+    .append('li')
+    .attr('style', `--color:${colors(idx)}`).attr('class', 'legend-item').html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); 
+});
