58159af80cbc7c33e62b1f8ee32a52e24e14f7d6
Aarav Agrawal <a7agrawal@ucsd.edu>
2026-05-08 13:38:15 -0700

    Lab 6 Submission

--- global.js
+++ global.js
@@ -19,6 +19,7 @@ let pages = [
   { url: "projects/", title: "Projects" },
   { url: "contact/", title: "Contact" },
   { url: "CV/", title: "CV" },
+  { url: "meta/", title: "Meta" },
   { url: "https://github.com/aarav1335", title: "GitHub" },
 ];
 
